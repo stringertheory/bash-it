bcf6b00d3027db5e93558b738c1e9e822d4d20f8
stringertheory <mike.stringer.internet@gmail.com>
2018-10-05 12:32:39 -0500

    updates

--- custom/aliases.bash
+++ custom/aliases.bash
@@ -11,9 +11,11 @@ alias decrap='rm -f *~; rm -f "#"*; rm -f *.pyc; rm -f *.pyo; c;'
 alias xmgrace='xmgrace -free -geometry 2500x1500 -param $HOME/.xmgrace_default'
 
 # web development
-alias simpleserver='python -m SimpleHTTPServer'
+alias simpleserver='$(which python3) -m http.server'
 
 # applications
 # alias julia='/Applications/Julia-0.3.3.app/Contents/Resources/julia/bin/julia'
 # alias qtgrace='/Applications/qtgrace.app/Contents/MacOS/qtgrace'
 # alias inkscape='/Applications/Inkscape.app/Contents/MacOS/Inkscape'
+
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
--- custom/virtualenvwrapper.bash
+++ custom/virtualenvwrapper.bash
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+eval "$(pyenv init -)"
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+
 # set up virtualenvwrapper
 WORKON_HOME=~/.virtualenvs
 mkdir -p ${WORKON_HOME}
-vew=/usr/local/bin/virtualenvwrapper_lazy.sh
-if [ -e ${vew} ]; then
-   source ${vew}
-fi
+# vew=/usr/local/bin/virtualenvwrapper_lazy.sh
+# if [ -e ${vew} ]; then
+#    source ${vew}
+# fi
+pyenv virtualenvwrapper_lazy
 
 # Call virtualenvwrapper's "workon" if .venv exists.  This is modified from--
 # http://justinlilly.com/python/virtualenv_wrapper_helper.html
